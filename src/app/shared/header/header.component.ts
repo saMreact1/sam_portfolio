f764ee6b8f9cddac6606898447306bbe82f55904
Sam <samueladeleke392@gmail.com>
2025-06-12 16:19:22 +0100

    Mobile responsiveness

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,32 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isNavOpen: boolean = false;
+  isDarkMode: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isNavOpen = false; // Close the menu on route change
+      }
+    });
+  }
 
   toggleNav() {
     this.isNavOpen = !this.isNavOpen
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
+
+  closeMenu() {
+    this.isNavOpen = false;
   }
 }
