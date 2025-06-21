81898230c3a58d060f5acbcfa82b7c18364cc984
Sam <samueladeleke392@gmail.com>
2025-06-21 19:10:55 +0100

    Contact page competed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,6 +42,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     MatButtonModule,
     MatTooltipModule,
     MatIconModule,
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  name: string = '';
+  email: string = '';
+  subject: string = "Samuel Adeleke, I'd like to hire you!"
+  message: string = '';
 
+  sendEmail() {
+    const emailBody = `Name: ${this.name}\nEmail: ${this.email}\nSubject: ${this.subject}\nMessage: ${this.message}`;
+    const mailtoLink = `mailto:samueladeleke302@gmail.com` + `?subject=${encodeURIComponent(this.subject)}` + `&body=${encodeURIComponent(emailBody)}`;
+    window.open(mailtoLink, '_blank');
+    this.resetForm();
+    alert('Your message has been sent! I will get back to you as soon as possible.');
+  }
+
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.subject = "Samuel Adeleke, I'd like to hire you!";
+    this.message = '';
+  }
 }
